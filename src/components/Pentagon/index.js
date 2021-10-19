10373ed451f6a5885feaca2d9f4f45fe5f13c061
Genevieve <vievenavales29@gmail.com>
2021-10-19 19:33:54 +0800

    adding attack components dynamically

--- src/components/Pentagon/index.js
+++ src/components/Pentagon/index.js
@@ -1,35 +1,18 @@
 import { Attack, Container, Wrapper } from "./Pentagon.styles";
 
 const Pentagon = () => {
+  const attacks = ["spock", "scissors", "paper", "rock", "lizard"];
   return (
     <Container>
       <Wrapper>
         <img src="images/bg-pentagon.svg" alt="pentagon background" />
-        <Attack className="spock">
-          <div>
-            <img src="images/icon-spock.svg" alt="spock" />
-          </div>
-        </Attack>
-        <Attack className="scissors">
-          <div>
-            <img src="images/icon-scissors.svg" alt="scissors" />
-          </div>
-        </Attack>
-        <Attack className="paper">
-          <div>
-            <img src="images/icon-paper.svg" alt="paper" />
-          </div>
-        </Attack>
-        <Attack className="rock">
-          <div>
-            <img src="images/icon-rock.svg" alt="rock" />
-          </div>
-        </Attack>
-        <Attack className="lizard">
-          <div>
-            <img src="images/icon-lizard.svg" alt="lizard" />
-          </div>
-        </Attack>
+        {attacks.map((attack) => (
+          <Attack className={attack}>
+            <div>
+              <img src={`images/icon-${attack}.svg`} alt={attack} />
+            </div>
+          </Attack>
+        ))}
       </Wrapper>
     </Container>
   );
