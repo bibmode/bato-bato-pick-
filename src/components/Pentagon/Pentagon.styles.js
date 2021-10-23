2f590d6d51d103491e3696904397fa553dfb3f6c
Genevieve <vievenavales29@gmail.com>
2021-10-23 17:10:45 +0800

    pentagon coins responsive shadows

--- src/components/Pentagon/Pentagon.styles.js
+++ src/components/Pentagon/Pentagon.styles.js
@@ -25,24 +25,24 @@ export const Wrapper = styled.div`
     left: 3%;
     bottom: clamp(11.5rem, 17vw, 19rem);
     background-image: linear-gradient(hsl(189, 58%, 57%), hsl(189, 59%, 53%));
-    box-shadow: 0 0.35rem rgba(64, 185, 206, 0.7);
+    box-shadow: 0 clamp(0.35rem, 1vw, 0.6rem) rgba(64, 185, 206, 0.7);
   }
   .scissors {
     top: clamp(-0.1rem, -20vw, -15rem);
     background-image: linear-gradient(hsl(40, 84%, 53%), hsl(39, 89%, 49%));
-    box-shadow: 0 0.35rem #ff7f00ba;
+    box-shadow: 0 clamp(0.35rem, 1vw, 0.6rem) #ff7f00ba;
   }
   .paper {
     right: 3%;
     bottom: clamp(11.5rem, 17vw, 19rem);
     background-image: linear-gradient(hsl(230, 89%, 65%), hsl(230, 89%, 62%));
-    box-shadow: 0 0.35rem rgba(72, 101, 244, 0.6);
+    box-shadow: 0 clamp(0.35rem, 1vw, 0.6rem) rgba(72, 101, 244, 0.6);
   }
   .rock {
     bottom: 0;
     right: clamp(4.5rem, 9vw, 9rem);
     background-image: linear-gradient(hsl(349, 70%, 56%), hsl(349, 71%, 52%));
-    box-shadow: 0 0.35rem rgba(220, 46, 78, 0.6);
+    box-shadow: 0 clamp(0.35rem, 1vw, 0.6rem) rgba(220, 46, 78, 0.6);
 
     img {
       height: 45%;
@@ -52,7 +52,7 @@ export const Wrapper = styled.div`
     bottom: 0;
     left: clamp(4.5rem, 9vw, 9rem);
     background-image: linear-gradient(hsl(261, 72%, 63%), hsl(261, 73%, 60%));
-    box-shadow: 0 0.35rem rgba(131, 79, 227, 0.6);
+    box-shadow: 0 clamp(0.35rem, 1vw, 0.6rem) rgba(131, 79, 227, 0.6);
   }
 `;
 
@@ -80,7 +80,7 @@ export const Attack = styled(motion.button)`
     align-items: center;
     justify-content: center;
     border-radius: 50%;
-    box-shadow: inset 0 0.3rem #cbcbcb;
+    box-shadow: inset 0 clamp(0.35rem, 0.5vw, 0.5rem) #cbcbcb;
 
     img {
       height: 50%;
